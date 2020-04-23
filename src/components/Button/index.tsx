edcbdf094a593d3798a879ebb6f0d9e34d7e9ce4
Pablo Satler <pablosatler@gmail.com>
2020-04-23 20:49:30 -0300

    chore: create components for Input and Button
    - install react-native-vector-icons as well and perform the necessary configs for Android
    - style both the Input and Button components

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { RectButtonProperties } from 'react-native-gesture-handler';
+
+import { Container, ButtonText } from './styles';
+
+interface ButtonProps extends RectButtonProperties {
+  children: string; // makind children mandatory and of type string because it is for the Text tag
+}
+
+const Button: React.FC<ButtonProps> = ({ children, ...rest }) => (
+  <Container {...rest}>
+    <ButtonText>{children}</ButtonText>
+  </Container>
+);
+
+export default Button;
