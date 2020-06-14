31edc8c202ffd4ab090bda6862eae4ca6dc66d4b
Pablo Satler <pablosatler@gmail.com>
2020-06-13 22:41:05 -0300

    chore: create basic structure for performing appointments

---
+++ src/pages/CreateAppointment/styles.ts
@@ -0,0 +1,37 @@
+import { Platform } from 'react-native';
+import styled, { css } from 'styled-components/native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+
+export const Container = styled.View`
+  flex: 1;
+`;
+
+export const Header = styled.View`
+  padding: 24px;
+  ${Platform.OS === 'ios' &&
+  // add the padding top only on ios devices
+  css`
+    padding-top: ${getStatusBarHeight() + 24}px;
+  `}
+  background: #28262e;
+
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const BackButton = styled.TouchableOpacity``;
+
+export const HeaderTitle = styled.Text`
+  color: #f4ede8;
+  font-size: 20px;
+  font-family: 'RobotoSlab-Medium';
+  margin-left: 16px;
+`;
+
+export const UserAvatar = styled.Image`
+  width: 56px;
+  height: 56px;
+  border-radius: 28px;
+  margin-left: auto;
+`;
