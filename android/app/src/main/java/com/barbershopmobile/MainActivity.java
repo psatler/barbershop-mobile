5ebd203d2914327500dfd1c8fce258894be97804
Pablo Satler <pablosatler@gmail.com>
2020-07-19 13:41:50 -0300

    feat: install react-native-splash-screen to delay display of Splash Screen
    - create layout folder in the res folder and inside, a file called layout_screen
    - hide splash screen in the JS part when everything has loaded up

--- android/app/src/main/java/com/barbershopmobile/MainActivity.java
+++ android/app/src/main/java/com/barbershopmobile/MainActivity.java
@@ -1,8 +1,17 @@
 package com.barbershopmobile;
 
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen; // Adicione esse import
+import android.os.Bundle; // Adicione esse import
 
 public class MainActivity extends ReactActivity {
+
+   // Adicione esse método
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+      SplashScreen.show(this);
+      super.onCreate(savedInstanceState);
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View, ActivityIndicator } from 'react-native';
+// import { View, ActivityIndicator } from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 
 import AuthRoutes from './auth.routes';
 import AppRoutes from './app.routes';
@@ -9,19 +10,23 @@ import { useAuth } from '../hooks/auth';
 const Routes: React.FC = () => {
   const { user, loading } = useAuth();
 
-  if (loading) {
-    return (
-      <View
-        style={{
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <ActivityIndicator size="large" color="#999" />
-      </View>
-    );
+  if (!loading) {
+    SplashScreen.hide();
   }
+
+  // if (loading) {
+  //   return (
+  //     <View
+  //       style={{
+  //         flex: 1,
+  //         justifyContent: 'center',
+  //         alignItems: 'center',
+  //       }}
+  //     >
+  //       <ActivityIndicator size="large" color="#999" />
+  //     </View>
+  //   );
+  // }
 
   return user ? <AppRoutes /> : <AuthRoutes />;
 };
